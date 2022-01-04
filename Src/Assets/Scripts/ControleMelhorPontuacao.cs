a925293dc2dcc1e97204159b94fac6b0b588ec56
FernandoPFH <51247535+FernandoPFH@users.noreply.github.com>
2022-01-04 00:16:31 -0300

    Criado Sistema De Pontuação

---
+++ Src/Assets/Scripts/ControleMelhorPontuacao.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControleMelhorPontuacao : MonoBehaviour
+{
+    // Variáveis Globais Acessiveis De Fora Da Classe
+    public static int melhorPontuacao = 0;
+}
--- Src/Assets/Scripts/SistemaDoJogo.cs
+++ Src/Assets/Scripts/SistemaDoJogo.cs
@@ -1,23 +1,49 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
+using System;
 
 public class SistemaDoJogo : MonoBehaviour
 {
+    // Variáveis Acessiveis De Fora Da Classe
+    public TMP_Text menuDePontuacao;
+    public TMP_Text menuDeMelhorPontuacao;
+
+    // Variáveis Acessiveis De Dentro Da Classe 
+    private int pontuacao = 0; 
+    private int melhorPontuacao = 0;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Pega A Melhor Pontuação Do Jogador
+        this.melhorPontuacao = ControleMelhorPontuacao.melhorPontuacao;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Calcula A Pontuação Atual Do Jogador
+        this.pontuacao = (int) Time.timeSinceLevelLoad;
+        // Mostra A Pontuação Na Tela
+        this.menuDePontuacao.text = this.pontuacao.ToString();
+
+        // Checa Para Mostrar Melhor Pontuação
+        if (this.pontuacao < this.melhorPontuacao) {
+            this.menuDeMelhorPontuacao.text = this.melhorPontuacao.ToString();
+        } else {
+            this.menuDeMelhorPontuacao.text = this.pontuacao.ToString();
+        }
+
     }
 
+    // Chamada Para Recomeçar O Jogo
     public void recomarJogo() {
+        // Se Pontuação Atual For Maior Que Melhor Pontuação, Atualiza Pontuação
+        if (this.pontuacao > this.melhorPontuacao) {
+            ControleMelhorPontuacao.melhorPontuacao = this.pontuacao;
+        }
+
         // Reinicia A Cena Do Jogo
         SceneManager.LoadScene("CenaDoJogo",LoadSceneMode.Single);
 
