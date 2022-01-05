f5e24d231a5c8ed8788f432215de2a06aaa79654
FernandoPFH <19.00499-0@maua.br>
2022-01-04 22:30:58 -0300

    Adicionado Efeito De Explosão Do Jogador

--- Src/Assets/Scripts/Jogador/MorteDoJogador.cs
+++ Src/Assets/Scripts/Jogador/MorteDoJogador.cs
@@ -6,6 +6,10 @@ public class MorteDoJogador : MonoBehaviour
 {
     // Variáveis Acessiveis De Fora Da Classe
     public GameObject menuMorteDoJogador;
+    public GameObject fogoDeExplosao;
+    public ParticleSystem fogoEsquerdo;
+    public ParticleSystem fogoCentral;
+    public ParticleSystem fogoDireito;
 
     // Start is called before the first frame update
     void Start()
@@ -23,6 +27,25 @@ public class MorteDoJogador : MonoBehaviour
     void OnCollisionEnter(Collision objeto) {
         // Checa Se A Tag Do Objeto é "Asteroide"
         if (objeto.gameObject.tag == "Asteroide") {
+
+            
+            // Para Animação Do Fogo Do Jogador
+            fogoEsquerdo.Stop(true,ParticleSystemStopBehavior.StopEmittingAndClear);
+            fogoCentral.Stop(true,ParticleSystemStopBehavior.StopEmittingAndClear);
+            fogoDireito.Stop(true,ParticleSystemStopBehavior.StopEmittingAndClear);
+
+            // Desaparece Com A Nave 
+            foreach (MeshRenderer meshRenderer in this.GetComponentsInChildren<MeshRenderer>())
+            {
+                meshRenderer.enabled = false;
+            }
+
+            // Começa Efeito De Explosão
+            foreach (ParticleSystem particleSystem in fogoDeExplosao.GetComponentsInChildren<ParticleSystem>())
+            {
+                particleSystem.Play();
+            }
+
             // Ativa O Menu De Morte
             menuMorteDoJogador.SetActive(true);
 
