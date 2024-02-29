6133b717a807e9246eb5902ae87f6c361161f461
FernandoPFH <51247535+FernandoPFH@users.noreply.github.com>
2024-02-28 23:24:53 -0300

    Configuração Build WebGL

--- Src/Assets/Scripts/Spawner/SpawnDeAsteroides.cs
+++ Src/Assets/Scripts/Spawner/SpawnDeAsteroides.cs
@@ -7,7 +7,7 @@ public class SpawnDeAsteroides : MonoBehaviour
 {
     // Variáveis Acessiveis De Fora Da Classe
     public float porcentagemDeEspacoUsado = 70;
-    public int tempoEntreSpawns = 2000;
+    public int tempoEntreSpawns = 2;
     public GameObject asteroidePequeno;
     public GameObject asteroideMedio;
     public GameObject asteroideGrande;
@@ -17,61 +17,71 @@ public class SpawnDeAsteroides : MonoBehaviour
     // Variáveis Acessiveis De Dentro Da Classe 
     private float extremidade;
 
+    private float lastSpawn;
+
     // Start is called before the first frame update
     void Start()
     {
         // Calcula As Extremidades No Eixo X
         this.extremidade = this.gameObject.GetComponentInChildren<MeshRenderer>().bounds.center.x + this.gameObject.GetComponentInChildren<MeshRenderer>().bounds.extents.x;
     
-        // Começa A Spawner Asteroides
+        // Spawna Primeiro Asteroide
         this.spawnarAsteroides();
+        lastSpawn = Time.time;
     }
 
-    private async void spawnarAsteroides() {
+    // Update is called once per frame
+    void Update()
+    {
+        // Checa Se Já Passou Mais Tempo Que O Delay
+        if (Time.time - lastSpawn < tempoEntreSpawns)
+            return;
 
-        // Spawna Asteroides Com Um Delay Entre Eles
-        while(this != null) {
-            var posicaoMax = this.extremidade * this.porcentagemDeEspacoUsado / 100;
+        // Spawna Asteroide
+        spawnarAsteroides();
+        lastSpawn = Time.time;
+    }
 
-            // Checa Se O Jogo Não Está Parado
-            if (Time.timeScale > 0f) {
-                // Escolhe Um Número Aleatório Entre 0 - 9
-                var escolhaDeTamanho = Random.Range(0, 10);
+    // Spawna Asteroides Com Um Delay Entre Eles
+    private void spawnarAsteroides() {
+        // Checa Se O Jogo Não Está Parado
+        if (Time.timeScale <= 0f)
+            return;
 
-                GameObject prefabEscolhido;
+        var posicaoMax = this.extremidade * this.porcentagemDeEspacoUsado / 100;
 
-                // Escolhe Qual Asteróide Usar Baseado No Número Escolhido
-                if (escolhaDeTamanho <= 4) {
-                    prefabEscolhido = asteroidePequeno;
-                } else if (escolhaDeTamanho <= 6) {
-                    prefabEscolhido = asteroideMedio;
-                } else {
-                    prefabEscolhido = asteroideGrande;
-                }
+        // Escolhe Um Número Aleatório Entre 0 - 9
+        var escolhaDeTamanho = Random.Range(0, 10);
 
-                // Escolhe Um Número Aleatório Entre 0 - 9
-                var escolhaDeCor = Random.Range(0, 10);
+        GameObject prefabEscolhido;
 
-                Material materialEscolhido;
+        // Escolhe Qual Asteróide Usar Baseado No Número Escolhido
+        if (escolhaDeTamanho <= 4) {
+            prefabEscolhido = asteroidePequeno;
+        } else if (escolhaDeTamanho <= 6) {
+            prefabEscolhido = asteroideMedio;
+        } else {
+            prefabEscolhido = asteroideGrande;
+        }
 
-                // Escolhe Qual Material Do Asteróide Usar Baseado No Número Escolhido
-                if (escolhaDeCor <= 4) {
-                    materialEscolhido = lavaVermelha;
-                } else {
-                    materialEscolhido = lavaAzul;
-                }
+        // Escolhe Um Número Aleatório Entre 0 - 9
+        var escolhaDeCor = Random.Range(0, 10);
 
-                // Instanciar O Asteroide
-                var asteroide = Instantiate(prefabEscolhido,new Vector3(Random.Range(-posicaoMax,posicaoMax),this.transform.position.y,this.transform.position.z),Quaternion.identity);
+        Material materialEscolhido;
 
-                // Colocar O Material No Asteroide
-                foreach (MeshRenderer meshRenderer in asteroide.GetComponentsInChildren<MeshRenderer>()){
-                    meshRenderer.material = materialEscolhido;
-                }
-            }
-        
-        
-            await Task.Delay(this.tempoEntreSpawns);
+        // Escolhe Qual Material Do Asteróide Usar Baseado No Número Escolhido
+        if (escolhaDeCor <= 4) {
+            materialEscolhido = lavaVermelha;
+        } else {
+            materialEscolhido = lavaAzul;
+        }
+
+        // Instanciar O Asteroide
+        var asteroide = Instantiate(prefabEscolhido,new Vector3(Random.Range(-posicaoMax,posicaoMax),this.transform.position.y,this.transform.position.z),Quaternion.identity);
+
+        // Colocar O Material No Asteroide
+        foreach (MeshRenderer meshRenderer in asteroide.GetComponentsInChildren<MeshRenderer>()){
+            meshRenderer.material = materialEscolhido;
         }
     }
 }
